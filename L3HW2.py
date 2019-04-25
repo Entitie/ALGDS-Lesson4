bb4f740f52b2b265077e3a17f625fe8472750a09
Entitie <entitie@yandex.ru>
2019-04-25 15:08:07 +0300

    Создал файлы с ДЗ.

--- L3HW2.py
+++ L3HW2.py
@@ -1,14 +1,17 @@
 # 2. Посчитать четные и нечетные цифры введенного натурального числа.
 # Например, если введено число 34560, то у него 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
+#
+# number = input('Введите натрульаное число: ')
+# even_digits = []
+# deven_digits = []
+# for el in number:
+#     if int(el) % 2:
+#         deven_digits.append(el)
+#     else:
+#         even_digits.append(el)
+#
+# print(f'У числа {number} количество чётных чисел равно {len(even_digits)}, и это {even_digits}')
+# print(f'А количество НЕчётных чисел равно {len(deven_digits)}, и это {deven_digits}')
 
-number = input('Введите натрульаное число: ')
-even_digits = []
-deven_digits = []
-for el in number:
-    if int(el) % 2:
-        deven_digits.append(el)
-    else:
-        even_digits.append(el)
-
-print(f'У числа {number} количество чётных чисел равно {len(even_digits)}, и это {even_digits}')
-print(f'А количество НЕчётных чисел равно {len(deven_digits)}, и это {deven_digits}')
+# Давайте проверим как лучше определить чётное число. Деление на 2, деление на 2 последнего числа,
+# или просто проверить является ли последнее число 0 или 2 или 4 или 6 или 8?
