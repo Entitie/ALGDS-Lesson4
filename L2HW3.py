ccd4278932a331e6777875a3772d32200d66034a
Entitie <entitie@yandex.ru>
2019-04-25 15:09:27 +0300

    HW done

--- L2HW3.py
+++ L2HW3.py
@@ -5,3 +5,27 @@
 # print(number[::-1])
 
 # Давайте проверим, что лучше число и срез?
+
+import timeit
+
+def srez():
+    K = '123456789'
+    # return K[::-1]
+
+
+# print(srez())
+
+def revert():
+    K = '123456789'
+    K = int(K)
+    result = 0
+    while K > 0:
+        result = result * 10 + K % 10
+        K = K // 10
+    # return result
+
+
+# print(revert())
+
+print(timeit.timeit('srez()', setup='from __main__ import srez', number=100000))
+print(timeit.timeit('revert()', setup='from __main__ import revert', number=100000))
--- L3HW2.py
+++ L3HW2.py
@@ -15,3 +15,71 @@
 
 # Давайте проверим как лучше определить чётное число. Деление на 2, деление на 2 последнего числа,
 # или просто проверить является ли последнее число 0 или 2 или 4 или 6 или 8?
+
+import timeit
+
+def str_even():
+    K = '123456789'
+    n = int(K[-1::])
+    if n % 2:
+        answer = 'Not even'
+    else:
+        answer = 'Even'
+    # return answer
+
+
+# print(str_even())
+
+
+def str_if_even():
+    K = '123456789'
+    if K[-1::] == '0' or K[-1::] == '2' or  K[-1::] == '4' or  K[-1::] == '6' or  K[-1::] == '8':
+        answer = 'Even'
+    else:
+        answer = 'Not even'
+    # return answer
+
+
+# print(str_if_even())
+
+def int_even():
+    K = '123456789'
+    K = int(K)
+    if K % 2:
+        answer = 'Not even'
+    else:
+        answer = 'Even'
+    # return answer
+
+
+# print(int_even())
+
+def int_last_even():
+    K = '123456789'
+    K = int(K) % 10
+    if K % 2:
+        answer = 'Not even'
+    else:
+        answer = 'Even'
+    # return answer
+
+
+# print(int_last_even())
+
+def int_if_last_even():
+    K = '123456789'
+    K = int(K) % 10
+    if K == 0 or K == 2 or K == 4 or K == 6 or K == 8:
+        answer = 'Even'
+    else:
+        answer = 'Not even'
+    # return answer
+
+
+# print(int_if_last_even())
+
+print(timeit.timeit('str_even()', setup='from __main__ import str_even', number=100000))
+print(timeit.timeit('str_if_even()', setup='from __main__ import str_if_even', number=100000))
+print(timeit.timeit('int_even()', setup='from __main__ import int_even', number=100000))
+print(timeit.timeit('int_last_even()', setup='from __main__ import int_last_even', number=100000))
+print(timeit.timeit('int_if_last_even()', setup='from __main__ import int_if_last_even', number=100000))
